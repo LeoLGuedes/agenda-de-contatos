cd28dda29fd0a6a6ee937277f63aa3ceed2480a7
Leonardo Guedes <leonardo.guedes.08@gmail.com>
2023-12-20 21:32:14 -0300

    Adicionado validador de números

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 const form = document.getElementById('container-form');
 
+const cell = [];
+
 let linhas = '';
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
+    adicionaLinha();
+})
+
+function adicionaLinha() {
     const inputNome = document.getElementById('input-nome');
     const inputCell = document.getElementById('input-cell');
 
+    if (cell.includes(inputCell.value)) {
+        alert(`O número de celular: ${inputCell.value} já foi inserido.`)
+    }
+    else {
+        cell.push(inputCell.value);
+        cell.push(parseFloat(inputCell.value));
 
     let linha = '<tr>';
     linha += `<td>${inputNome.value}</td>`;
@@ -15,11 +27,12 @@ form.addEventListener('submit', function(e) {
     linha += `</tr>`;
 
     linhas += linha;
+    }
 
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas;
 
     inputNome.value = '';
     inputCell.value = '';
-})
+}
 
